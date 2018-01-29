f295d8ebeee2d47b9dc5f24bfc0fc48915fbd80e
Rafał Brzoska <rafal@brzoska.net.pl>
2018-01-29 14:27:05 +0100

    arrow toggle

--- fp-app/src/app/products/product-list-item.component.ts
+++ fp-app/src/app/products/product-list-item.component.ts
@@ -7,7 +7,12 @@ import {Product} from '../models/Product';
     <div>{{product.name}}</div>
     <div *ngIf="product.id === expandedProductId">{{product.description}}</div>
     <button type="button" class="btn btn-danger"
-            (click)="toggle()">Toggle</button>
+            (click)="toggle()">      
+      Toggle
+    <i class="fa" [ngClass]="{
+                'fa-arrow-up':product.id === expandedProductId,
+                'fa-arrow-down':product.id !== expandedProductId
+    }"></i></button>
     <button type="button" 
             (click)="sellProduct()"
             class="btn btn-primary">Sprzedaj</button>
