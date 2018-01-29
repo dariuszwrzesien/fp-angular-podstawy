9fd3a19b96ae7136d59b3bf11dabec44880fe66f
Rafał Brzoska <rafal@brzoska.net.pl>
2018-01-29 12:54:25 +0100

    components

--- fp-app/src/app/app.module.ts
+++ fp-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductListItemComponent } from './products/product-list-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    ProductListComponent,
+    ProductListItemComponent
   ],
   imports: [
     BrowserModule
---
+++ fp-app/src/app/products/product-list-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-item',
+  template: `
+    <p>
+      product-list-item works!
+    </p>
+  `,
+  styles: []
+})
+export class ProductListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
