4474612f3b4760d8c21fb4ce4578f7ffc526cae6
Rafał Brzoska <rafal@brzoska.net.pl>
2018-01-30 14:14:15 +0100

    switchMap

--- fp-app/src/app/products/product-edit.component.ts
+++ fp-app/src/app/products/product-edit.component.ts
@@ -2,30 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {ProductService} from '../product.service';
 import {Product} from '../models/Product';
+import 'rxjs/add/operator/switchMap';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-product-edit',
   template: `
     <p>
-      Product: {{product | json}}
+      Product: {{product$ | async | json}}
     </p>
   `,
   styles: []
 })
 export class ProductEditComponent implements OnInit {
 
-  product: Product;
+  product$: Observable<Product>;
   constructor(private activatedRoute: ActivatedRoute,
               private productService: ProductService) { }
 
   ngOnInit() {
-    this.activatedRoute.params
-      .subscribe((params:Params) => {
-        this.productService.getProductById(params['id'])
-          .subscribe(
-            p => this.product = p
-          );
-      })
+    this.product$ = this.activatedRoute.params
+      .switchMap((params: Params) => this.productService.getProductById(params['id']));
   }
 
 }
